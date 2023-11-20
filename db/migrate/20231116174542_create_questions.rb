26ee3e2b30a95d0fb41ffbd33a33e4f01e22bc47
MrSaharok <zhelticov@gmail.com>
2023-11-20 21:00:52 +0700

    Commit

--- db/migrate/20231116174542_create_questions.rb
+++ db/migrate/20231116174542_create_questions.rb
@@ -2,7 +2,7 @@ class CreateQuestions < ActiveRecord::Migration[6.1]
   def change
     create_table :questions do |t|
       t.string :title
-      t.string :body
+      t.text :body
 
       t.timestamps
     end
