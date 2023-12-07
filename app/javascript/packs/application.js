eab7cd4a303906dcd194ad3b6fd0e182bee10bde
MrSaharok <zhelticov@gmail.com>
2023-12-08 00:43:58 +0700

    fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("@popperjs/core")
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'jquery'
 import 'bootstrap'
 import "channels"
