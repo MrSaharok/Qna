dad88119b3df9d1bda6270450186dc24ada6c459
MrSaharok <zhelticov@gmail.com>
2024-01-18 21:28:32 +0700

    commit 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
-         :trackable, :validatable, :omniauthable, omniauth_providers: [:github]
+         :trackable, :validatable, :omniauthable, omniauth_providers: %i[github vkontakte]
 
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
   has_many :rewards, dependent: :destroy
   has_many :votes, dependent: :destroy
+  has_many :authorizations, dependent: :destroy
 
   def author_of?(object)
     self.id == object.user_id
-  end
-
-  def self.find_for_ouath(auth)
   end
 end
