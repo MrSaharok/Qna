0954dabaaf5926e93fb7553d41ca5259a541eadc
MrSaharok <zhelticov@gmail.com>
2024-01-18 21:38:57 +0700

    refactoring

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,6 +37,7 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include ControllerHelpers, type: :controller
   config.include FeatureHelpers, type: :feature
+  config.include OmniauthMacros
 
   Capybara.javascript_driver = :selenium_chrome_headless
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
@@ -78,3 +79,6 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+OmniAuth.config.test_mode = true
+
