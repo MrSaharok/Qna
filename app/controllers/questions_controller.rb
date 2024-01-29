382605ec74e7746b8417232c9f434377b250b805
MrSaharok <zhelticov@gmail.com>
2024-01-30 00:01:58 +0700

    commit 4

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,7 @@ class QuestionsController < ApplicationController
 
   before_action :authenticate_user!, except: %i[index show]
   before_action :load_question, only: %i[show edit destroy update ]
-
+  before_action :load_subscription, only: %i[show update]
   after_action :publish_question, only: :create
 
   authorize_resource
@@ -53,6 +53,10 @@ class QuestionsController < ApplicationController
     @question = Question.with_attached_files.find(params[:id])
     gon.question_id = @question.id
     gon.question_user_id = @question.user_id
+  end
+
+  def load_subscription
+    @subscription = @question.subscriptions.find_by(user: current_user)
   end
 
   def question_params
