bd0260767ee25c3f2ef80b76640104b19342cf01
MrSaharok <zhelticov@gmail.com>
2024-02-10 01:33:19 +0700

    JS WELL DONE

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import 'channels'
 import 'bootstrap'
+import { Tooltip, Popover } from "bootstrap"
 //= require skim
 import '../vote'
 import '../answers'
@@ -35,4 +36,52 @@ document.addEventListener("turbolinks:load", () => {
     var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
         return new Popover(popoverTriggerEl)
     })
+})
+
+let App = App || {}
+App.cable = createConsumer()
+
+App.cable.subscriptions.create('AnswersChannel', {
+    connected: function () {
+        let question_id = gon.question_id;
+        return this.perform('follow', {question_id: question_id});
+    },
+    received: function (data) {
+        if (gon.user_id !== data.answer.user_id) {
+            $('.answers').append(JST['templates/answer']({
+                answer: data.answer,
+                links: data.links,
+                rating: data.rating
+            }));
+        }
+    }
+})
+
+App.cable.subscriptions.create('CommentsChannel', {
+    connected:function () {
+        let question_id = gon.question_id;
+        return this.perform('follow', { question_id: question_id });
+    },
+    received: function (data) {
+        if (gon.user_id !== data.comment.user_id) {
+            let resourceName = data.comment.commentable_type,
+                resourceId = data.comment.commentable_id,
+                newComment = JST['templates/comment']({comment: data.comment});
+
+            if (resourceName === 'Question') {
+                $(`#question_${resourceId} .comments`).append(newComment);
+            } else if (resourceName === 'Answer') {
+                $(`#answer_${resourceId} .comments`).append(newComment);
+            }
+        }
+    }
+})
+
+App.cable.subscriptions.create('QuestionsChannel', {
+    connected: function () {
+        return this.perform('follow');
+    },
+    received: function (data) {
+        $('.questions').append(JST['templates/question'](data));
+    }
 })
